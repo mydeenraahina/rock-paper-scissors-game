d610ccc5c28265856c51744318196d0a9b017116
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-10-01 20:05:39 -0500

    first

---
+++ rockpaperscissorsgame.py
@@ -0,0 +1,40 @@
+import random
+list=["rock","paper","scissors"]
+x=random.choice(list)
+def play():
+    print("Loading...")
+    choice1=input("[rock,paper,scissors]\nchoose any one of the following to play the game :")
+    print("your choice :",choice1)
+    print("computer choice :",x)
+    if choice1=="rock" and x=="scissors":
+        print("u won the game")
+    elif choice1=="rock" and x=="paper":
+         print("computer won the game")
+    elif choice1=="rock" and x=="rock":
+       print("both of ur choice are same")
+    if choice1=="paper" and x=="scissors":
+         print("computer won the game")
+    elif choice1=="paper" and x=="stone":
+         print("u won the game")
+    elif choice1=="paper" and x=="paper":
+         print("both of ur choice are same")
+    if choice1=="scissors" and x=="paper":
+         print("u won the game")
+    elif choice1=="scissors" and x=="rock":
+         print("computer won the game")
+    elif choice1=="scissors" and x=="scissors":
+          print("both of ur choice are same")
+print("welcome to rock,paper,scissors game")
+choice=input("Do yoy want to paly the game :")
+if choice=="yes":
+    play()
+else:
+    print("exit")
+while True:
+    choice2=input("do you continue this game :")
+    if choice2=="yes":
+        play()
+    else:
+       print("exit")
+       break
+
