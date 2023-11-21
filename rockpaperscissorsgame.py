7380c5068f1bc0522d3751b7212e96ca1cdc49db
MydeenRaahina <145376500+mydeenraahina@users.noreply.github.com>
2023-11-21 12:28:36 -0500

    Update rockpaperscissorsgame.py

--- rockpaperscissorsgame.py
+++ rockpaperscissorsgame.py
@@ -1,52 +1,43 @@
 import random
-list=["rock","paper","scissors"]
-x=random.choice(list)
-def play():
-    def rock():
-        print("your choice :",choice1)
-        print("computer choice :",x)
-        if choice1=="rock" and x=="scissors":
-            print("u won the game")
-        elif choice1=="rock" and x=="paper":
-             print("computer won the game")
-        elif choice1=="rock" and x=="rock":
-           print("both of ur choice are same")
-    def paper():
-        print("your choice :",choice1)
-        print("computer choice :",x)
-        if choice1=="paper" and x=="scissors":
-             print("computer won the game")
-        elif choice1=="paper" and x=="stone":
-             print("u won the game")
-        elif choice1=="paper" and x=="paper":
-             print("both of ur choice are same")
-    def scissors():
-        print("your choice :",choice1)
-        print("computer choice :",x)
-        if choice1=="scissors" and x=="paper":
-             print("u won the game")
-        elif choice1=="scissors" and x=="rock":
-             print("computer won the game")
-        elif choice1=="scissors" and x=="scissors":
-             print("both of ur choice are same")
-             print("Loading...")
-    choice1=input("[rock,paper,scissors]\nchoose any one of the following to play the game :")
-    if choice1=="rock":
-        rock()
-    elif choice1=="paper":
-        paper()
-    else:
-        scissors()
-print("welcome to rock,paper,scissors game")
-choice=input("Do yoy want to paly the game :")
-if choice=="yes":
-    play()
-else:
-    print("type no to exit/yes to continue the game")
-while True:
-    choice2=input("do you continue this game :")
-    if choice2=="yes":
-        play()
-    else:
-       print("exit")
-       break
+def play_game():
+    def rock_paper_scissors():
+        def continue_game():
+            while True:
+                input_to_continue=input(f"do you continue this game [yes\\ no]:")
+                if input_to_continue.lower()=="yes":
+                    rock_paper_scissors()
+                elif input_to_continue.lower()=="no":
+                    print(f"Exit!")
+                    break
+                else:
+                    print(f"Enter a  valid input ")
+                    rock_paper_scissors()
+        user_choice=input("[rock,paper,scissors]\nchoose any one of the following to play the game :").lower()
+        if user_choice in['rock','paper','scissors']:
+            print(f"{user_choice=}")
+            print(f"{computer_choice=}")
+            if (user_choice=="rock" and computer_choice=="scissors" or
+                user_choice == "paper" and computer_choice=="rock" or
+                user_choice == "scissors" and computer_choice=="paper"):
+                    print("user won the game")
+            elif user_choice==computer_choice:
+                print(f"Game Tied")
+            else:
+                print(f"computer won the game")
+        else:
+            print(f"Enter a  valid input ")
+            rock_paper_scissors()
+        continue_game()
+    print(f"rock,paper,scissors game")
+    list_value=["rock","paper","scissors"]
+    computer_choice=random.choice(list_value)
+    while True:
+        getting_input_to_start=input(f"Do you want to play the game[yes\\no] :")
+        if  getting_input_to_start.lower()=="yes":
+            rock_paper_scissors()
+        elif  getting_input_to_start.lower()=="no":
+            print(f"Exit!")
+            break
+        else:
+            print(f"Enter a  valid input ")
+play_game()
